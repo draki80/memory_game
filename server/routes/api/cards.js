e9fa2e1956132753244dec9a3ef26d4715811013
Slusalica800 <draki80@gmail.com>
2021-02-28 17:13:37 +0100

    fix: small refactor

--- server/routes/api/cards.js
+++ server/routes/api/cards.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.get('/:cardNumber', (req, res) => {
     let response = []
     for (let step = 0; step < req.params.cardNumber; step++) {
-        let randomNumber = Math.floor(Math.random() * Math.floor(1000))
+        let randomNumber = Math.floor(Math.random() * Math.floor(100))
         response.push(randomNumber)
       }
       res.send(response)
